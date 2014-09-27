b36ccd07859f3252a4cf8b0cc8d764bad6489b37
Yamamoto Yuji <whosekiteneverfly@gmail.com>
2014-09-27 18:14:40 +0900

    simply send the contents of the readme file to the driver process, discarding the output from that

--- src/Test/ReadmeTest.hs
+++ src/Test/ReadmeTest.hs
@@ -1,3 +1,23 @@
-module Test.ReadmeTest where
+module Test.ReadmeTest
+  ( readmeTestWith
+  )
+where
 
+import Data.Conduit
+import Data.Conduit.Process
+import qualified Data.Conduit.Binary as CB
+import qualified Data.Conduit.List as CL
+import Control.Monad.Trans.Resource
+import System.IO
+import Control.Monad (forM_)
+import qualified Data.ByteString.Char8 as BSC8
 import Test.ReadmeTest.Internal
+
+readmeTestWith :: FilePath -> [FilePath] -> IO ()
+readmeTestWith driverPath readmePaths = do
+  forM_ readmePaths $ \ readmePath -> runResourceT $ do
+    (toProcess, ClosedStream, ClosedStream, cph) <- streamingProcess (proc driverPath [])
+    sourceSourceCodeLines readmePath $$ toProcess
+
+sourceSourceCodeLines :: MonadResource m => FilePath -> Source m BSC8.ByteString
+sourceSourceCodeLines fp = CB.sourceFile fp $= CB.lines
