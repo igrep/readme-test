357dc7bf6856b212bd079be474848e5b4722dc0e
Yamamoto Yuji <whosekiteneverfly@gmail.com>
2014-09-28 19:42:52 +0900

    try send input again with debug conduit

--- src/Test/ReadmeTest.hs
+++ src/Test/ReadmeTest.hs
@@ -19,18 +19,22 @@ import Test.ReadmeTest.Internal
 readmeTestWith :: FilePath -> [FilePath] -> IO ()
 readmeTestWith driverPath readmePaths = do
   forM_ readmePaths $ \ readmePath -> do
-    (toProcess, Inherited, Inherited, cph) <- streamingProcess (proc driverPath [])
+    (toProcess, fromProcess, Inherited, cph) <- streamingProcess (proc driverPath [])
 
-    let input = runResourceT $ sourceSourceCodeLines readmePath $$ toProcess
-    {-let output = fromProcess $$ CB.sinkHandle stdout-}
+    let input = runResourceT $ CB.sourceFile readmePath $= conduitDebug $$ toProcess
+    let output = fromProcess $$ CB.sinkHandle stdout
 
-    {-_ <- runConcurrently $-}
-      {-Concurrently input *>-}
-      {-Concurrently output *>-}
-      {-Concurrently (waitForStreamingProcess cph)-}
+    _ <- runConcurrently $
+      Concurrently input *>
+      Concurrently output *>
+      Concurrently (waitForStreamingProcess cph)
 
-    input
     return ()
 
 sourceSourceCodeLines :: MonadResource m => FilePath -> Source m BSC8.ByteString
 sourceSourceCodeLines fp = CB.sourceFile fp $= CB.lines
+
+conduitDebug :: MonadIO m => Conduit BSC8.ByteString m BSC8.ByteString
+conduitDebug = awaitForever $ \ chunk -> do
+  liftIO $ BSC8.putStrLn chunk
+  return chunk
